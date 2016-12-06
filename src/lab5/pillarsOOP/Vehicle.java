bc966fe42b8bc3cf2e7d2e24e134de93d5b815a7
flo <flo.macicasan@gmail.com>
2016-12-06 21:14:01 +0200

    adds Object methods override

--- src/lab5/pillarsOOP/Vehicle.java
+++ src/lab5/pillarsOOP/Vehicle.java
@@ -1,10 +1,12 @@
 package lab5.pillarsOOP;
 
+import javafx.geometry.Pos;
+
 /**
  * @author flo
  * @since 06/12/2016.
  */
-public abstract class Vehicle {
+public abstract class Vehicle implements Movable {
     private int id;
     public void moves() {
         System.out.println("Vehicle moves");
@@ -12,11 +14,35 @@ public abstract class Vehicle {
 
     public abstract void start();
 
+    public Position getPosition() {
+        return new Position();
+    }
+
+//    @Override
+    public String tozString() {
+        return "This is a Vehicle with id " + id;
+    }
+
+    @Override
+    public int hashCode() {
+        return 5;
+    }
+
     public static void main(String[] args) {
         Car car = new Car();
         car.start();
         car.moves();
+        Movable movable = car;
+        Vehicle v = car;
+        movable.moves();
+        v.moves();
+        System.out.println(car.toString());
     }
+}
+
+interface Movable {
+    void moves();
+    Position getPosition();
 }
 
 class Car extends Vehicle {
